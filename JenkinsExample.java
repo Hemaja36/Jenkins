6b9a9e59166017f46cea97d399b9b986194be990
Hemaja36 <2020wa86867@wilp.bits-pilani.ac.in>
2024-06-07 12:47:05 +0000

    Update JenkinsExample.java

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -2,7 +2,7 @@ public class JenkinsExample
   {
     public static void main(String args [])
     {
-      System.out.printin("welcome to jenkins");
+      System.out.println("welcome to jenkins");
 
     }
 
